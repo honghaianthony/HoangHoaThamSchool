8fcdfb2889a1692d358f90bf6f06655bbdd2abd5
Anthony Hai <namsaoquan@gmail.com>
2022-09-20 22:43:24 +0700

    fix: seo not found robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,17 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import image from "@astrojs/image";
+import robotsTxt from "astro-robots-txt";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://hoang-hoa-tham-school.vercel.app/",
-    integrations: [tailwind(), react(), image(), sitemap()],
+    experimental: {
+        integrations: true,
+    },
+    integrations: [tailwind(), react(), image(), sitemap(), robotsTxt()],
     vite: {
         ssr: {
             external: ["svgo"],
